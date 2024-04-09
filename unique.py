158481523182e81e312e6040edfff17f628a9334
youze <youzel@126.com>
2024-04-09 23:08:11 +0800

    Auto commit by script

--- unique.py
+++ unique.py
@@ -1,17 +1,21 @@
 def deduplicate_http(file_path):
-    # 读取文件内容到列表，并只保留包含 "http" 的行
     http_lines = []
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
             if 'http' in line:
                 http_lines.append(line)
 
-    # 去重
     unique_lines = list(set(http_lines))
 
-    # 将去重后的内容覆盖回原文件
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.writelines(unique_lines)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        original_content = file.readlines()
+
+    if original_content == unique_lines:
+        print("文件内容未发生变化，无需修改。")
+    else:
+        # 将去重后的内容覆盖回原文件
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.writelines(unique_lines)
 
 
 file_path = "file.txt"
