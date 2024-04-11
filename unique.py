042cdf836288f29a00d3ecf712b2c51b5f86486a
liangyouze <youzel@126.com>
2024-04-11 09:56:06 +0800

    Auto commit by script

--- unique.py
+++ unique.py
@@ -1,16 +1,19 @@
 def deduplicate_http(file_path):
     http_lines = []
+    flag = False
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
             if 'http' in line:
                 http_lines.append(line)
+            if '<' in line:
+                flag = True
 
     unique_lines = list(set(http_lines))
 
     with open(file_path, 'r', encoding='utf-8') as file:
         original_content = file.readlines()
 
-    if original_content == unique_lines:
+    if original_content == unique_lines and flag:
         print("文件内容未发生变化，无需修改。")
     else:
         # 将去重后的内容覆盖回原文件
