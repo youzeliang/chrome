33767551d3b2011df8e02a01424217a2b0772d28
youze <youzel@126.com>
2024-04-22 21:00:28 +0800

    update file

--- unique.py
+++ unique.py
@@ -2,15 +2,24 @@ from git import Repo
 import os
 
 
-def deduplicate_http(file_path):
+def deduplicate_http(file_name):
+    file_path = '/Users/youzeliang/dev/code/chrome'
+    dirfile = os.path.abspath(file_path)
+    repo = Repo(dirfile)
+    g = repo.git
+    g.config('--global', 'user.name', 'youze')
+    g.config('--global', 'user.email', 'youzel@126.com')
+
+    g.push()
+
     http_lines = []
     flag = False
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_name, 'r', encoding='utf-8') as file:
         for line in file:
             if 'http' in line:
                 http_lines.append(line)
             if '<' in line:
-                flag = True
+                flag = False
 
     unique_lines = list(set(http_lines))
     #
@@ -22,15 +31,8 @@ def deduplicate_http(file_path):
         print("文件内容未发生变化，无需修改。")
     else:
         # 将去重后的内容覆盖回原文件
-        with open(file_path, 'w', encoding='utf-8') as file:
+        with open(file_name, 'w', encoding='utf-8') as file:
             file.writelines(unique_lines)
-
-        file_path = '/Users/youzeliang/dev/code/chrome'
-
-        dirfile = os.path.abspath(file_path)
-        repo = Repo(dirfile)
-
-        g = repo.git
 
         g.add('--all')
         g.commit('-m', 'update file')
